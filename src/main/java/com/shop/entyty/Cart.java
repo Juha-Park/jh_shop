5bfa2e367e8a55d66e9b9f2c966bc2ba1860327a
Juha-Park <juha0531@gmail.com>
2022-08-10 23:36:45 +0900

    import 수정

--- src/main/java/com/shop/entyty/Cart.java
+++ src/main/java/com/shop/entyty/Cart.java
@@ -18,7 +18,7 @@ public class Cart extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY) //일대일 매핑.
+    @OneToOne(fetch = FetchType.LAZY) // 일대일 매핑.
     @JoinColumn(name="member_id") //매핑할 외래키를 지정.
     private Member member;
 
