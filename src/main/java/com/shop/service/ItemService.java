fe5b628a151d80ed569e82bcd06041590e6f2f92
Juha-Park <juha0531@gmail.com>
2022-08-10 18:15:15 +0900

    Initial Commit

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -1,24 +1,26 @@
 package com.shop.service;
 
-
 import com.shop.dto.ItemFormDto;
-import com.shop.dto.ItemImgDto;
-import com.shop.dto.ItemSearchDto;
-import com.shop.dto.MainItemDto;
 import com.shop.entyty.Item;
 import com.shop.entyty.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+import com.shop.dto.ItemImgDto;
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+
+import com.shop.dto.ItemSearchDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.shop.dto.MainItemDto;
 
 @Service
 @Transactional
@@ -26,6 +28,7 @@ import java.util.List;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
     private final ItemImgService itemImgService;
 
     private final ItemImgRepository itemImgRepository;
@@ -42,11 +45,11 @@ public class ItemService {
             itemImg.setItem(item);
 
             //첫 번째 이미지일 경우 대표 상품 이미지 여부 값을 "Y"로 세팅. 나머지 상품 이미지는 "N"로 설정.
-            if(i==0) {
+            if(i == 0)
                 itemImg.setRepImgYn("Y");
-            }else {
+            else
                 itemImg.setRepImgYn("N");
-            }
+
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
         }
 
@@ -63,22 +66,23 @@ public class ItemService {
         List<ItemImgDto> itemImgDtoList = new ArrayList<>();
 
         //조회한 ItemImg 엔티티를 ItemImgDto 객체로 만들어서 리스트에 추가.
-        for(ItemImg itemImg : itemImgList){
+        for (ItemImg itemImg : itemImgList) {
             ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
             itemImgDtoList.add(itemImgDto);
         }
 
         //상품의 아이디를 통해 상품 엔티티를 조회. 존재하지 않을 경우 EntityNodFoundException을 발생시킴.
-        Item item = itemRepository.findById(itemId).orElseThrow(EntityNotFoundException::new);
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
         ItemFormDto itemFormDto = ItemFormDto.of(item);
         itemFormDto.setItemImgDtoList(itemImgDtoList);
         return itemFormDto;
     }
 
     public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
-
         //상품 수정
-        Item item = itemRepository.findById(itemFormDto.getId()).orElseThrow(EntityNotFoundException::new);
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
         item.updateItem(itemFormDto);
 
         //상품 이미지 아이디 리스트를 조회.
@@ -86,10 +90,11 @@ public class ItemService {
 
         //이미지 등록
         for(int i=0;i<itemImgFileList.size();i++){
-            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+            itemImgService.updateItemImg(itemImgIds.get(i),
+                    itemImgFileList.get(i));
         }
-        return item.getId();
 
+        return item.getId();
     }
 
     //상품 조회 조건과 페이지 정보를 파라미터로 받아 상품 데이터를 조회.
@@ -103,4 +108,5 @@ public class ItemService {
     public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
         return itemRepository.getMainItemPage(itemSearchDto, pageable);
     }
+
 }
