2eeb0f03416a35c62cfe21a0764ccfee86e110e5
Juha-Park <juha0531@gmail.com>
2022-08-11 14:39:05 +0900

    다대일 매핑 fetchtype 변경

--- src/main/java/com/shop/entyty/ItemImg.java
+++ src/main/java/com/shop/entyty/ItemImg.java
@@ -27,7 +27,7 @@ public class ItemImg extends BaseEntity{
     private String repImgYn;  //대표 이미지 여부
 
     //Item 엔티티와 다대일 단방향 관계로 매핑. 지연 로딩을 설정하여 매핑된 Item 엔티티 정보가 필요할 경우 데이터를 조회하도록 함.
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "item_id")
     private Item item;
 
