c07d690646c28b9f73bce1814a5c1229d24a376f
Juha-Park <juha0531@gmail.com>
2022-08-11 18:13:01 +0900

    Commit

--- src/main/java/com/shop/controller/itemController.java
+++ src/main/java/com/shop/controller/itemController.java
@@ -55,7 +55,7 @@ public class itemController {
         try{
             itemService.saveItem(itemFormDto, itemImgFileList);
         } catch (Exception e){
-            model.addAttribute("errorMessage", "상품 등록 중 에러가 발행하였습니다.");
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
             return "item/itemForm";
         }
 
